c098327897f04003da3ef13bc183a4f110d73652
Sreenath Sirobhushanam <sreenathsirobhushanam@Sreenaths-MacBook-Pro.local>
2022-03-24 23:21:13 +0530

    Included MFE changes to Remote1 and ContainerApp

--- MFE/Remote1/src/App.tsx
+++ MFE/Remote1/src/App.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 function App() {
     return (<div>
-        <h2>This is the container</h2>
+        <h2>This is MFE1</h2>
         <h3>Date : {new Date().toDateString()}</h3>
     </div>)
 }
